58c5a676f83dfbd1930db090d30751d5b03a5255
Jonbonbroski <jonbonbroski@gmail.com>
2019-09-14 21:48:03 -0700

    preparing models with boiler plate code

---
+++ models/Headline.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+
+var HeadlineSchema = new Schema({
+  
+  title: String,
+  
+  body: String
+});
+
+var Note = mongoose.model("Headline", HeadlineSchema);
+
+module.exports = HeadlineSchema;
